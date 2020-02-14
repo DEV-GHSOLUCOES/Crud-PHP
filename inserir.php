508da1bb382f3e3cf2458e25cc3526cb285747d7
DevGuima <glecioheybel@hotmail.com>
2020-02-14 14:56:27 -0300

    commit de formulario em html e script php para inserção dos dados em banco

---
+++ inserir.php
@@ -0,0 +1,29 @@
+<?php
+
+ $link =  mysql_connect("localhost", "root",  "", "test");
+
+ //checando a conexao
+ if (!$link) {
+
+ 		printf("Erro ao conectar ao banco de dados: ", mysqli_connect_errno());
+ 	}
+ //Definindo as variaveis para receber os dados do formulario
+ $aluno = $_POST['aluno'];
+ $disciplina = $_POST['disciplina'];
+ $nota1 = $_POST['nota1'];
+ $nota2 = $_POST['nota2'];
+ $nota3 = $_POST['nota3'];
+
+
+//criando a query em SQL para inserir os dados no banco
+ $query = "INSERT INTO etepac ( aluno, disciplina, nota1, nota2, nota3) 
+ VALUES ('$aluno', '$disciplina', '$nota1', '$nota2', '$nota3')";
+
+//executando o comando SQL
+ mysql_query($link, $query);
+
+ //exibe mensagem de confirmação
+ echo "Dados Inseridos" 
+
+
+?>
